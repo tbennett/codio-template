92fbba1c1914c416f1f1546c98308307719821b7
tbennett <betabennett@gmail.com>
2014-12-10 07:50:48 +0000

    debufggin

--- js/main.js
+++ js/main.js
@@ -36,14 +36,14 @@ function onDeviceReady() {
 // This method accepts a Position object, which contains the
 // current GPS coordinates
 var onSuccessGeo = function(position) {
-    /*alert('Latitude: '          + position.coords.latitude          + '\n' +
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
           'Longitude: '         + position.coords.longitude         + '\n' +
           'Altitude: '          + position.coords.altitude          + '\n' +
           'Accuracy: '          + position.coords.accuracy          + '\n' +
           'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
           'Heading: '           + position.coords.heading           + '\n' +
           'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');*/
+          'Timestamp: '         + position.timestamp                + '\n');
 };
 
 
@@ -60,6 +60,7 @@ function onError(error) {
 function onSuccess(imageURI) {
     var image = document.getElementById('myImage');
     image.src = imageURI;
+    alert(imageURI);
 }
 
 
